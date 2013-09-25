64e3fd565bdaeb2ac58cbfa834ae51fe807f6f19
Arnaudchiffe <arnaud.chiffe@gmail.com>
2013-09-25 14:31:59 +0200

    Commit by Arnaud

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="icon" type="image/x-icon" href="GrindHouse_Leather.ico" />
         <title>GrindHouse Leather France. Maroquinerie, sacs à main</title>
     </head>
     <body>
         <?php
-        
+            
         ?>
     </body>
 </html>
