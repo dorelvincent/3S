06ca0bcd32dcb7edd2605a8e1c94e774ef3b8565
Arnaudchiffe <arnaud.chiffe@gmail.com>
2013-09-25 15:54:20 +0200

    Commit by Arnaud

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
     	<title>GrindHouse Leather France. Maroquinerie, sacs à main</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <meta name="keywords" content="site, e-commerce, commerce, online, chaussure, chaussures, chapeau, chapeaux, ceinture, ceintures, sacs, sac,sacs, à, main, dosportefeuille, porte, monnaie, feuille, cartes, carte, clé, clés, clef, clefs, serviette, serviettes, maroquinerie, maroquineries, shopping, etui, gant, gants, bracelet, bracelets"
+        <meta name="keywords" content="site, e-commerce, commerce, online, chaussure, chaussures, chapeau, chapeaux, ceinture, ceintures, sacs, sac,sacs, à, main, dosportefeuille, porte, monnaie, feuille, cartes, carte, clé, clés, clef, clefs, serviette, serviettes, maroquinerie, maroquineries, shopping, etui, gant, gants, bracelet, bracelets, grind, grindhouse, leather"
         <meta name="description" content="site de e-commerce en maroquinerie, sacs à main" />
         <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
     </head>
