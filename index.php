bdb9d3482341e3ad9c708bc609d0a30fb7fe5cfe
Arnaudchiffe <arnaud.chiffe@gmail.com>
2013-09-25 15:52:26 +0200

    Commit by Arnaud

--- index.php
+++ index.php
@@ -3,12 +3,13 @@
     <head>
     	<title>GrindHouse Leather France. Maroquinerie, sacs à main</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <meta name="description" content="Maroquinerie, sacs à main" />
+        <meta name="keywords" content="site, e-commerce, commerce, online, chaussure, chaussures, chapeau, chapeaux, ceinture, ceintures, sacs, sac,sacs, à, main, dosportefeuille, porte, monnaie, feuille, cartes, carte, clé, clés, clef, clefs, serviette, serviettes, maroquinerie, maroquineries, shopping, etui, gant, gants, bracelet"
+        <meta name="description" content="site de e-commerce en maroquinerie, sacs à main" />
         <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
     </head>
     <body>
         <?php
-        
+            
         ?>
     </body>
 </html>
